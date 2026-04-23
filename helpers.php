0adf85a3109ce4ba75e08aae9c78e2228a5030d5
alienjel22 <alienjel1222@gmail.com>
2026-04-24 02:22:36 +0800

    Initial commit — ws03 project setup

---
+++ helpers.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Get the base path
+ * 
+ * 
+ * @param string $path
+ * @return string
+ */
+
+function basePath($path = '')
+{
+    return __DIR__ . '/' . $path;
+}
+
+/**
+ * Load view
+ * 
+ * @param string $name
+ * @return void
+ */
+
+function loadView($name)
+{
+    require basePath("views/{$name}.view.php");
+}
+
+/**
+ * Load partials
+ * 
+ * @param string $name
+ * @return void
+ */
+
+function loadPartial($name)
+{
+    $partialPath = basePath("views/partials/{$name}.php");
+    
+    if (file_exists($partialPath)) {
+        require $partialPath;
+    } else {
+        echo "Partial '{$name}' not found.";
+    }
+}
