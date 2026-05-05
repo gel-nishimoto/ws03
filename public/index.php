f99a1889494db2dc37d1dacd3aaaee7fcbbdb412
alienjel22 <alienjel1222@gmail.com>
2026-05-05 09:38:24 +0800

    update: index

--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
 <?php
 require '../helpers.php';
 require basePath('Router.php');
+require basePath('Database.php');
+
+$config = require basePath('config/db.php');
+$db = new Database($config);
 
 $router = new Router();
 
