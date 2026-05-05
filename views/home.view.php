855c7f2989f7515b1f2e697b605cfb65da6cc2fd
alienjel22 <alienjel1222@gmail.com>
2026-05-05 11:30:19 +0800

    feat: update job listing details to include dynamic location and tags

--- views/home.view.php
+++ views/home.view.php
@@ -19,16 +19,15 @@
                     <ul class="my-4 bg-gray-100 p-4 rounded">
                         <li class="mb-2"><strong>Salary:</strong><?= formatSalary($listing->salary) ?></li>
                         <li class="mb-2">
-                            <strong>Location:</strong> New York
+                            <strong>Location:</strong><?= $listing->city ?>, <?= $listing->state ?>
                             <span
                                 class="text-xs bg-blue-500 text-white rounded-full px-2 py-1 ml-2">Local</span>
                         </li>
                         <li class="mb-2">
-                            <strong>Tags:</strong> <span>Development</span>,
-                            <span>Coding</span>
+                            <strong>Tags:</strong> <?= $listing->tags ?>
                         </li>
                     </ul>
-                    <a href="details.html"
+                    <a href="/listings/<?= $listing->id ?>"
                         class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
                         Details
                     </a>
