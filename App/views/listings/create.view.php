99cd27b936902b35b6bc4483d5e93aba38bc11cb
alienjel22 <alienjel1222@gmail.com>
2026-05-16 23:27:30 +0800

    feat: Implement user authorization/authentication with registration and login

--- App/views/listings/create.view.php
+++ App/views/listings/create.view.php
@@ -4,16 +4,12 @@
 <!-- Post a Job Form Box -->
 <section class="flex justify-center items-center mt-20">
     <div class="bg-white p-8 rounded-lg shadow-md w-full md:w-600 mx-6">
-        <h2 class="text-4xl text-center font-bold mb-4">Create Job Listing</h2>=
+        <h2 class="text-4xl text-center font-bold mb-4">Create Job Listing</h2>
         <form method="POST" action="/listings">
             <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">
                 Job Info
             </h2>
-            <?php if (isset($errors)) : ?>
-                <?php foreach ($errors as $error) : ?>
-                    <div class="message bg-red-100 my-3"><?= $error ?></div>
-                <?php endforeach; ?>
-            <?php endif; ?>
+            <?php loadPartial('errors', ['errors' => $errors ?? [] ]) ?>
             <div class="mb-4">
                 <input
                     type="text"
@@ -26,8 +22,8 @@
                 <textarea
                     name="description"
                     placeholder="Job Description"
-                    class="w-full px-4 py-2 border rounded focus:outline-none">
-                    <?= $listing['description'] ?? ''  ?></textarea>
+                    class="w-full px-4 py-2 border rounded focus:outline-none"
+                    value="<?= $listing['description'] ?? '' ?>"></textarea>
             </div>
             <div class="mb-4">
                 <input
