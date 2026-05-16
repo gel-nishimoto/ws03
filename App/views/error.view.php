a37c0e7bf5f6b939db157bc7053945afef10d31d
alienjel22 <alienjel1222@gmail.com>
2026-05-16 16:19:27 +0800

    feat: implement error handling and refactor routing to use controllers

--- App/views/error.view.php
+++ App/views/error.view.php
@@ -4,16 +4,16 @@
 
 <section>
     <div class="container mx-auto p-4 mt-4">
-        <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">404</div>
+        <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">Error</div>
         <p class="text-center h2-style marg-bot">
-            Page not found.
+            <?= $status ?>
         </p>
         <p class="text-center mt-6 text-lg font-medium text-pretty sm:text-xl/8 marg-bot">
-            Sorry, we couldn’t find the page you’re looking for.
+            <?= $message ?>
         </p>
         <div class="flex justify-center">
-            <a href="/" class="text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium text-indigo-700 bg-yellow-500">
-                Go back home
+            <a href="/listings" class="text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium text-indigo-700 bg-yellow-500">
+                Go back to listings
             </a>
         </div>
     </div>
--- Framework/Router.php
+++ Framework/Router.php
@@ -2,16 +2,30 @@
 
 namespace Framework;
 
+use App\Controllers\ErrorController;
+
 class Router
 {
     protected $routes = [];
 
-    public function registerRoute($method, $uri, $controller) 
+    /**
+     * Add a new route
+     * 
+     * @param string $method
+     * @param string $uri
+     * @param string @action
+     * @return void
+     */
+
+    public function registerRoute($method, $uri, $action)
     {
+        list($controller, $controllerMethod) = explode('@', $action);
+
         $this->routes[] = [
             'method' => $method,
             'uri' => $uri,
-            'controller' => $controller
+            'controller' => $controller,
+            'controllerMethod' => $controllerMethod
         ];
     }
 
@@ -21,7 +35,7 @@ class Router
      * @param string $uri
      * @param string $controller
      */
-    
+
     public function get($uri, $controller)
     {
         $this->registerRoute('GET', $uri, $controller);
@@ -65,20 +79,6 @@ class Router
     }
 
     /**
-     * Load error page
-     * 
-     * @param int $httpCode
-     * @return void 
-     */
-
-    public function error($httpCode = 404)
-    {
-        http_response_code($httpCode);
-        loadView("error/{$httpCode}");
-        exit;
-    }
-
-    /**
      * 
      * Route the request
      * 
@@ -87,15 +87,49 @@ class Router
      * @return void
      */
 
-    public function route($uri, $method)
+    public function route($uri)
     {
+        $requestMethod = $_SERVER['REQUEST_METHOD'];
         foreach ($this->routes as $route) {
-            if ($route['uri'] === $uri && $route['method'] === $method) {
-                require basePath('App/' . $route['controller']);
-                return;
+            // Split the current URI into segments
+            $uriSegments = explode('/', trim($uri, '/'));
+
+            // Split the route
+            $routeSegments = explode('/', trim($route['uri'], '/'));
+
+            $match = true;
+
+            if (count($uriSegments) === count($routeSegments) && strtoupper($route['method'] === $requestMethod)) {
+                $params = [];
+
+                $match = true;
+
+                for ($i = 0; $i < count($uriSegments); $i++) {
+                    //If the uwi fo not match and there is no value between the {id}
+                    if ($routeSegments[$i] !== $uriSegments[$i] && !preg_match('/\{(.+?)\}/', $routeSegments[$i])) {
+                        $match = false;
+                        break;
+                    }
+                    // Check for param and add to $params array
+                    if (preg_match('/\{(.+?)\}/', $routeSegments[$i], $matches)) {
+                        $params[$matches[1]] = $uriSegments[1];
+                    }
+                }
+
+                if ($match) {
+                    // Extract controller and controller method
+                    $controller = 'App\\Controllers\\' . $route['controller'];
+                    $controllerMethod = $route['controllerMethod'];
+
+                    // Instantiate controller class
+                    $controllerInstance = new $controller();
+                    $controllerInstance->$controllerMethod($params);
+                    return;
+                }
             }
         }
-        $this->error();
+
+
+        ErrorController::notFound();
     }
-    
 }
